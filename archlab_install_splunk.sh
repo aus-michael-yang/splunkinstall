f2f85b3ef387194adaf5cd98798f440779d534bb
aus-michael-yang <aus.michael.yang@gmail.com>
2021-12-06 07:45:29 +1100

    Update archlab_install_splunk.sh
    Don't need to create the user archStudent for the lab

--- archlab_install_splunk.sh
+++ archlab_install_splunk.sh
@@ -12,7 +12,7 @@
  sudo tar -xvzf $INSTALL_FILE
  sudo rm -rf $INSTALL_FILE
 
- sudo useradd -m -r archStudent
+ ##sudo useradd -m -r archStudent
 
  sudo /opt/splunk/bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt --seed-passwd $PASSWORD
  sudo /opt/splunk/bin/splunk stop -auth admin:$PASSWORD
