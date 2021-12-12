c2a41831be9896fce23c480dcac44d68da20b5f2
aus-michael-yang <aus.michael.yang@gmail.com>
2021-12-13 10:15:58 +1100

    Update install_splunkforwarder.sh

--- install_splunkforwarder.sh
+++ install_splunkforwarder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
  
- WGET_CMD="wget -O splunkforwarder-8.1.6-c1a0dd183ee5-Linux-x86_64.tgz https://download.splunk.com/products/universalforwarder/releases/8.1.6/linux/splunkforwarder-8.1.6-c1a0dd183ee5-Linux-x86_64.tgz"
+ WGET_CMD="wget -O splunkforwarder-8.2.3-c1a0dd183ee5-Linux-x86_64.tgz https://download.splunk.com/products/universalforwarder/releases/8.2.3/linux/splunkforwarder-8.2.3-c1a0dd183ee5-Linux-x86_64.tgz"
  INSTALL_FILE="splunkforwarder-*"
  PASSWORD="Password123"
 
