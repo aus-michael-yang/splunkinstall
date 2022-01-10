63ed4b252e5bc0622177c6e6755fb1224a395fe5
aus-michael-yang <aus.michael.yang@gmail.com>
2022-01-10 13:29:14 +1100

    Update archlab_install_splunk.sh

--- archlab_install_splunk.sh
+++ archlab_install_splunk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
  ##WGET_CMD="wget -O splunk-8.2.3-cd0848707637-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-Linux-x86_64.tgz"
- WGET_CMD="wget -O splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz 'https://download.splunk.com/products/splunk/releases/8.2.4/linux/splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz'"
+ WGET_CMD="wget -O splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.4/linux/splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz"
  
  INSTALL_FILE="splunk-*"
  PASSWORD="splk"
@@ -13,7 +13,7 @@
  sudo tar -xvzf $INSTALL_FILE
  sudo rm -rf $INSTALL_FILE
 
- #sudo useradd -m -r splunk
+ sudo useradd -m -r splunk
 
  sudo /opt/splunk/bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt --seed-passwd $PASSWORD
  sudo /opt/splunk/bin/splunk stop -auth admin:$PASSWORD
