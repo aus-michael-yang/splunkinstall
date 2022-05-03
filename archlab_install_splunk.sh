01fc1f4615da226c416f6d4652dd45cb3cbfb6c3
aus-michael-yang <aus.michael.yang@gmail.com>
2022-05-03 14:47:57 +1000

    Update archlab_install_splunk.sh

--- archlab_install_splunk.sh
+++ archlab_install_splunk.sh
@@ -1,8 +1,8 @@
 t#!/bin/sh
 
  ##WGET_CMD="wget -O splunk-8.2.3-cd0848707637-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-Linux-x86_64.tgz"
- WGET_CMD="wget -O splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.4/linux/splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz"
- 
+ ##WGET_CMD="wget -O splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.4/linux/splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz"
+ WGET_CMD="wget -O splunk-8.2.6-a6fe1ee8894b-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.6/linux/splunk-8.2.6-a6fe1ee8894b-Linux-x86_64.tgz"
  INSTALL_FILE="splunk-*"
  PASSWORD="ExpertInsight"
 
@@ -13,10 +13,11 @@ t#!/bin/sh
  sudo tar -xvzf $INSTALL_FILE
  sudo rm -rf $INSTALL_FILE
 
- sudo useradd -m -r splunk
+ #sudo useradd -m -r splunk
 
  sudo /opt/splunk/bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt --seed-passwd $PASSWORD
  sudo /opt/splunk/bin/splunk stop -auth admin:$PASSWORD
- sudo chown -R splunk:splunk /opt/splunk
- sudo -u splunk /opt/splunk/bin/splunk restart
- sudo /opt/splunk/bin/splunk enable boot-start -user splunk
+ #sudo chown -R splunk:splunk /opt/splunk
+ sudo chown -R ubuntu:ubuntu /opt/splunk
+ sudo -u ubuntu /opt/splunk/bin/splunk restart
+ sudo /opt/splunk/bin/splunk enable boot-start -user ubuntu
