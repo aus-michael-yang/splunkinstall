a168c09091f64c110499ab65b59970acc827c90d
aus-michael-yang <aus.michael.yang@gmail.com>
2022-09-29 23:50:37 +1000

    Update archlab_install_splunk.sh

--- archlab_install_splunk.sh
+++ archlab_install_splunk.sh
@@ -2,7 +2,8 @@
 
  ##WGET_CMD="wget -O splunk-8.2.3-cd0848707637-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-Linux-x86_64.tgz"
  ##WGET_CMD="wget -O splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.4/linux/splunk-8.2.4-87e2dda940d1-Linux-x86_64.tgz"
- WGET_CMD="wget -O splunk-8.2.6-a6fe1ee8894b-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.6/linux/splunk-8.2.6-a6fe1ee8894b-Linux-x86_64.tgz"
+ ##WGET_CMD="wget -O splunk-8.2.6-a6fe1ee8894b-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/8.2.6/linux/splunk-8.2.6-a6fe1ee8894b-Linux-x86_64.tgz"
+ WGET_CMD="wget -O splunk-9.0.1-82c987350fde-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/9.0.1/linux/splunk-9.0.1-82c987350fde-Linux-x86_64.tgz"
  INSTALL_FILE="splunk-*"
  PASSWORD="ExpertInsight"
 
